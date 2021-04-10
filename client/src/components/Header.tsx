ab4f28a28d2a737d5d830ffcefb749337e972da7
joneirikboska <joneirikboska@gmail.com>
2021-04-10 10:39:25 +0200

    Change header animations. Slight refactor of optional styling.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,8 +4,13 @@ import { gsap } from 'gsap'
 
 const Header = () => {
   useEffect(() => {
-    gsap.to('.header', { opacity: 1, duration: 1 })
-    gsap.to('.arrow-down', { opacity: 1, duration: 1, delay: 1 })
+    gsap.to('.studio-conflux', { opacity: 1, duration: 1, delay: 1 })
+    gsap.to('.studio-conflux-description', {
+      opacity: 0.7,
+      duration: 1,
+      delay: 2,
+    })
+    gsap.to('.arrow-down', { opacity: 1, duration: 1, delay: 2 })
   })
 
   function scrollDown() {
@@ -23,7 +28,6 @@ const Header = () => {
         justify='center'
         align='center'
         flexDirection='column'
-        opacity='0'
         transition='0.4s opacity linear'
       >
         <Heading
@@ -31,15 +35,17 @@ const Header = () => {
           as='h1'
           fontFamily='qigong'
           textAlign='center'
+          opacity='0'
           fontSize={{ base: '2rem', md: '3rem', lg: '4rem' }}
         >
           Studio Conflux
         </Heading>
         <Text
+          className='studio-conflux-description'
           textAlign='center'
           pt={4}
           fontSize={{ base: '1rem', md: '1.2rem', lg: '1.5rem' }}
-          opacity='0.7'
+          opacity='0'
         >
           Composition for Arts & Media // Music Production
           <br />
