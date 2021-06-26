a358a1ff6f37d716bc4031854a5c7b37b49b7133
joneirikboska <joneirikboska@gmail.com>
2021-06-26 20:35:24 +0200

    Restructure header component, create simple loading indicator.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,91 +1,91 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { Text, Box, Flex, Heading, useMediaQuery } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
+import { useState } from 'react'
 
-const Logo = () => {
+const Header = () => {
   const [isLargerThan650] = useMediaQuery('(min-width: 650px)')
-  const [visible, setVisible] = useState(false)
   const elementRef = useRef<SVGSVGElement | null>(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const svgElem = elementRef.current
 
     svgElem!.addEventListener('load', () => {
-      setVisible(true)
+      setLoading(false)
     })
+    //eslint-disable-next-line
   }, [])
 
-  return (
-    <>
-      <Box visibility={visible ? 'visible' : 'hidden'}>
-        <svg
-          ref={elementRef}
-          className='animate-filter'
-          width='100%'
-          height='100%'
-          style={{ display: 'none' }}>
-          <defs>
-            <filter id='wavy' filterUnits='userSpaceOnUse' x='0' y='0'>
-              <feTurbulence
-                id='wave-animation'
-                numOctaves='2'
-                seed='19'
-                baseFrequency='0.02 0.0645034'></feTurbulence>
-              <feDisplacementMap
-                scale={isLargerThan650 ? '12' : '6'}
-                in='SourceGraphic'></feDisplacementMap>
-            </filter>
-            <animate
-              attributeName='baseFrequency'
-              xlinkHref='#wave-animation'
-              dur='10s'
-              keyTimes='0;0.5;1'
-              values='0.2 0.04;0.2 0.07;0.2 0.04'
-              repeatCount='indefinite'></animate>
-          </defs>
-        </svg>
-        <Heading
-          className='studio-conflux'
-          as='h1'
-          fontFamily='qigong'
-          opacity='1'
-          filter="url('#wavy')"
-          textAlign='center'
-          fontSize={{ base: '1.5rem', md: '3rem', lg: '4rem' }}>
-          Studio
-          <br />
-          Conflux
-        </Heading>
-        <Text
-          className='studio-conflux-description'
-          pt={isLargerThan650 ? 6 : 2}
-          textAlign='center'
-          fontFamily='futura-pt'
-          fontSize={{ base: '1rem', md: '1.2rem', lg: '1.5rem' }}
-          opacity='0.7'>
-          Composition, Music Production and Sound Design
-          <br />
-          for Arts & Media
-        </Text>
-      </Box>
-    </>
-  )
-}
-
-const Header = () => {
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}>
+      {loading && (
+        <Flex height='100vh' justify='center' align='center'>
+          <Text fontFamily='futura-pt'>Loading...</Text>
+        </Flex>
+      )}
       <Flex
+        visibility={loading ? 'hidden' : 'visible'}
         className='header'
         height='70vh'
         justify='center'
         align='center'
         flexDirection='column'
         transition='0.4s opacity linear'>
-        <Logo />
+        <Box>
+          <svg
+            ref={elementRef}
+            className='animate-filter'
+            width='100%'
+            height='100%'
+            style={{ display: 'none' }}>
+            <defs>
+              <filter id='wavy' filterUnits='userSpaceOnUse' x='0' y='0'>
+                <feTurbulence
+                  id='wave-animation'
+                  numOctaves='2'
+                  seed='19'
+                  baseFrequency='0.02 0.0645034'></feTurbulence>
+                <feDisplacementMap
+                  scale={isLargerThan650 ? '12' : '6'}
+                  in='SourceGraphic'></feDisplacementMap>
+              </filter>
+              <animate
+                attributeName='baseFrequency'
+                xlinkHref='#wave-animation'
+                dur='10s'
+                keyTimes='0;0.5;1'
+                values='0.2 0.04;0.2 0.07;0.2 0.04'
+                repeatCount='indefinite'></animate>
+            </defs>
+          </svg>
+          <Heading
+            className='studio-conflux'
+            as='h1'
+            fontFamily='qigong'
+            opacity='1'
+            filter="url('#wavy')"
+            textAlign='center'
+            fontSize={{ base: '1.5rem', md: '3rem', lg: '4rem' }}>
+            Studio
+            <br />
+            Conflux
+          </Heading>
+          <Text
+            className='studio-conflux-description'
+            pt={isLargerThan650 ? 6 : 2}
+            textAlign='center'
+            fontFamily='futura-pt'
+            fontSize={{ base: '1rem', md: '1.2rem', lg: '1.5rem' }}
+            opacity='0.7'>
+            Composition, Music Production and Sound Design
+            <br />
+            for Arts & Media
+          </Text>
+        </Box>
       </Flex>
     </motion.div>
   )
