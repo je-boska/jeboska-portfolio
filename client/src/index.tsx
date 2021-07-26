ec6095939b8b72f76c25ec1350764c8d67eab214
joneirikboska <joneirikboska@gmail.com>
2021-07-26 18:16:45 +0200

    Import experimental 100vh solution.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import './index.css'
 
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import { Fonts } from './fonts'
