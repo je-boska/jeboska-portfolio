af24a08a20bed83476b7a4d6e972e75fedb419f1
je-boska <joneirikboska@gmail.com>
2021-12-20 19:50:27 +0100

    Remove body

--- client/src/components/ProjectText.tsx
+++ client/src/components/ProjectText.tsx
@@ -1,12 +1,11 @@
-import { Box, Heading } from '@chakra-ui/react'
-import React from 'react'
-import BlockContent from '@sanity/block-content-to-react'
+import { Box, Heading } from "@chakra-ui/react";
+import React from "react";
 
 interface ProjectTextProps {
-  title: string
-  body: string
-  isLargerThan650: boolean
-  first: boolean
+  title: string;
+  body: string;
+  isLargerThan650: boolean;
+  first: boolean;
 }
 
 const ProjectText: React.FC<ProjectTextProps> = ({
@@ -18,7 +17,7 @@ const ProjectText: React.FC<ProjectTextProps> = ({
   // css for BlockContent component
   const css = `
 .project-text h1 {
-    padding-bottom: ${isLargerThan650 ? '15px' : '10px'};
+    padding-bottom: ${isLargerThan650 ? "15px" : "10px"};
     line-height: 1;
     max-width: 100%;
     text-align: center;
@@ -41,36 +40,38 @@ const ProjectText: React.FC<ProjectTextProps> = ({
     font-size: 1.2rem;
   }
 }
-`
+`;
 
   return (
     <>
       <Box
-        id={first ? 'first-project-text' : undefined}
-        zIndex='-1'
-        width='100%'
+        id={first ? "first-project-text" : undefined}
+        zIndex="-1"
+        width="100%"
         padding={4}
-        marginTop='150px'
-        justify='center'
-        position='sticky'
-        top={2}>
+        marginTop="150px"
+        justify="center"
+        position="sticky"
+        top={2}
+      >
         <style>{css}</style>
         <Box
-          className='project-text to-fade'
-          opacity='0'
-          transition='0.4s opacity linear'>
+          className="project-text to-fade"
+          opacity="0"
+          transition="0.4s opacity linear"
+        >
           <Heading
-            as='h1'
-            letterSpacing={`${!isLargerThan650 ? '0.3rem' : '1.2rem'}`}
-            ml={`${!isLargerThan650 ? '0.3rem' : '1.2rem'}`}
-            fontSize={{ base: '1rem', md: '1.4rem', lg: '1.7rem' }}>
+            as="h1"
+            letterSpacing={`${!isLargerThan650 ? "0.3rem" : "1.2rem"}`}
+            ml={`${!isLargerThan650 ? "0.3rem" : "1.2rem"}`}
+            fontSize={{ base: "1rem", md: "1.4rem", lg: "1.7rem" }}
+          >
             {title.toUpperCase()}
           </Heading>
-          <BlockContent blocks={body} />
         </Box>
       </Box>
     </>
-  )
-}
+  );
+};
 
-export default ProjectText
+export default ProjectText;
