64f72945a5a4563f56114dd84cf6cb270bcf76b0
je-boska <joneirikboska@gmail.com>
2021-12-20 21:30:23 +0100

    Change clients list

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import { Box, Text } from '@chakra-ui/layout'
+import React from "react";
+import { Box, Text } from "@chakra-ui/layout";
 
 const Contact = () => {
   return (
     <>
       <Box
         m={8}
-        opacity='0.8'
-        fontSize={{ base: '1rem', md: '1.2rem', lg: '1.3rem' }}
+        opacity="0.8"
+        fontSize={{ base: "1rem", md: "1.2rem", lg: "1.3rem" }}
       >
-        <Text maxW='600px' fontFamily='futura-pt'>
+        <Text maxW="600px" fontFamily="futura-pt">
           Studio Conflux is a Berlin sound design, composition, music production
           and mixing studio, run by Tim Roth, Philipp Hülsenbeck and Jon Eirik
           Boska.
           <br />
           <br />
-          Clients include Adidas, Acqua Di Parma, *Wallpaper Mag, 032c,
-          Converse, SieMatics, Pop-Kultur, Forecast Festival.
+          Clients include Adidas, Converse, Glossier You, Tom Ford, Acqua Di
+          Parma, Siematics, GQ Germany and *Wallpaper Magazine.
           <br />
           <br />
           Get in touch
           <br />
-          hello@studioconflux.com
+          info@studioconflux.com
           <br />
           <br />
           Imprint
         </Text>
       </Box>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
